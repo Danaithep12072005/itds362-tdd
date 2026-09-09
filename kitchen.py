ba9b381ec2a55559614442bae2de29e18a4c0a80
Danaithep12072005 <man007659558@gmail.com>
2026-09-09 20:32:56 +0700

    A4: add equality comparison

--- kitchen.py
+++ kitchen.py
@@ -1,7 +1,14 @@
-# kitchen.py (แก้บรรทัด return)
+# kitchen.py (อัปเดตให้เป็นแบบนี้)
 class Quantity:
     def __init__(self, amount):
         self.amount = amount
 
     def times(self, multiplier):
         return Quantity(self.amount * multiplier)
+
+    # เพิ่ม 2 เมทอดนี้เข้ามา
+    def __eq__(self, other):
+        return self.amount == other.amount
+
+    def __repr__(self):
+        return f"Quantity({self.amount})"
