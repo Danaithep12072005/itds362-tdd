a24a8cd2f4e94060d6f53885a633e279b7681c01
Danaithep12072005 <man007659558@gmail.com>
2026-09-09 20:23:42 +0700

    A2: multiplication, triangulated

--- test_kitchen.py
+++ test_kitchen.py
@@ -5,3 +5,8 @@ def test_multiplication():
     flour = Quantity(200)
     flour.times(3)
     assert flour.amount == 600
+    # test_kitchen.py (เพิ่มโค้ดนี้ต่อท้าย)
+def test_multiplication_by_two():
+    flour = Quantity(200)
+    flour.times(2)
+    assert flour.amount == 400
